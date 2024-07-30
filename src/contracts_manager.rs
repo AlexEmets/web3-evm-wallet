569f402e9947a0dcaa1f2eaab3c32fec8b1fc8a8
Oleksandr Yemets <alexxandryemets@gmail.com>
2024-07-30 13:57:14 +0300

    Hardcoded SC info was moved to a separate config file

--- src/contracts_manager.rs
+++ src/contracts_manager.rs
@@ -1,8 +1,14 @@
 use crate::smart_contract::SmartContract;
+use std::fs;
+use serde_json::Value;
 
 pub struct ContractsManager {
     pub contracts_list:Vec<SmartContract>
 } 
+
+impl ContractsManager {
+    pub const CONFIG_PATH: &'static str = "/home/oleksandryemets/Documents/Studying/CryptoWallet/src/smart_contracts_info.json";
+}
 
 impl ContractsManager {
     pub fn new() -> Self {
@@ -18,22 +24,25 @@ impl ContractsManager {
         self.contracts_list.iter().find(|contract| contract.get_name() == name)
     }
 
-    // use only in constructor/new method 
+    // Parse `smart_contracts_info` config fileto extract all SC info. Used generally in constructor/new method 
     fn get_smart_contracts_list() -> Vec<SmartContract> {
-        let mut result_list: Vec<SmartContract> = Vec::new();
+        let config_data = fs::read_to_string(ContractsManager::CONFIG_PATH)
+            .expect("Unable to read smart contracts configuration file.");
+        let json_value: Value = serde_json::from_str(&config_data)
+            .expect("JSON was not well-formatted.");
     
-        result_list.push(SmartContract::new(
-            "Counter".to_string(),
-            "0xc6BCF9F0eaD0291e9e6D0cbD4aA4ca4Fa751707B".to_string(),
-            "/home/oleksandryemets/Documents/Studying/CryptoWallet/src/contracts/ABIs/counter_abi.json".to_string()
-        ));
-
-        result_list.push(SmartContract::new(
-            "Voting".to_string(),
-            "address".to_string(),
-            "/home/oleksandryemets/Documents/Studying/CryptoWallet/src/contracts/ABIs/voting_abi.json".to_string()
-        ));
-
+        let mut result_list: Vec<SmartContract> = Vec::new();
+        
+        if let Value::Array(contracts) = json_value {
+            for contract in contracts {
+                let name = contract["name"].as_str().unwrap().to_string();
+                let address = contract["address"].as_str().unwrap().to_string();
+                let abi_path = contract["abi_path"].as_str().unwrap().to_string();
+    
+                result_list.push(SmartContract::new(name, address, abi_path));
+            }
+        }
+    
         result_list
     }
 
